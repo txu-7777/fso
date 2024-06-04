57f0233de6e5a330e5832476c1fd7f3e36c1bf19
txu-7777 <157735433+txu-7777@users.noreply.github.com>
2024-06-04 14:49:26 -0700

    Finish module

--- backend/index.js
+++ backend/index.js
@@ -35,32 +35,30 @@ app.get("/api/notes/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
-
-  if (body.content === undefined) {
-    return response.status(400).json({
-      error: "content missing",
-    });
-  }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
   });
 
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
 });
 
 app.put("/api/notes/:id", (request, response, next) => {
-  const body = request.body;
-  const note = {
-    content: body.content,
-    important: body.important,
-  };
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  const { content, important } = request.body;
+  
+  Note.findByIdAndUpdate(
+    request.params.id,
+    {content, important}, // the note
+    { new: true, runValidators: true, context: "query" }
+    )
     .then((updatedNote) => {
       response.json(updatedNote);
     })
@@ -80,6 +78,8 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message);
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
   next(error);
 };
--- backend/models/note.js
+++ backend/models/note.js
@@ -13,7 +13,11 @@ mongoose
   });
 
 const noteSchema = new mongoose.Schema({
-  content: String,
+  content: {
+    type: String,
+    minLength: 5,
+    required: true,
+  },
   important: Boolean,
 });
 noteSchema.set("toJSON", {
